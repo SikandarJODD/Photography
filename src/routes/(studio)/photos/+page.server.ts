9a1860d951cc8fea70ff47c3c6de0a2a9f7b1e2f
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2024-03-31 15:23:13 +0530

    Update Browser Cache

--- src/routes/(studio)/photos/+page.server.ts
+++ src/routes/(studio)/photos/+page.server.ts
@@ -2,9 +2,9 @@ import { db } from "$lib/server";
 import { posts } from "$lib/server/schema";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ locals }) => {
+export const load: PageServerLoad = async ({ setHeaders }) => {
     let allposts = await db.select().from(posts);
-
+    setHeaders({ "Cache-Control": "max-age=3600" });
     return {
         uposts: allposts
     }
